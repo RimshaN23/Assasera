1d3019b2ae0c8dbd8801e37ba012ba99f509ea2c
Rimsha <rimsha732@gmail.com>
2024-03-25 20:55:52 +0500

    customer request ui

--- app/src/main/java/com/yolo/assasera/Home.kt
+++ app/src/main/java/com/yolo/assasera/Home.kt
@@ -3,7 +3,6 @@ package com.yolo.assasera
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.ActionBarDrawerToggle
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -53,7 +52,7 @@ class Home : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.home, menu)
+        menuInflater.inflate(R.menu.option_menu, menu)
         return true
     }
 
