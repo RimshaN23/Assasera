04e75d65ad98fbbef1e7a81e33233f0a2734dd09
Rimsha <rimsha732@gmail.com>
2024-03-25 14:27:30 +0500

    customer request ui

---
+++ app/src/main/java/com/yolo/assasera/CustomersRequests.kt
@@ -0,0 +1,11 @@
+package com.yolo.assasera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomersRequests : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customers_requests)
+    }
+}
